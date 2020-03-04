9853481c1781370165c10342e3aab564949a937e
EL MERROUNI Marouane <melmerrouni@norsys.fr>
2020-03-04 16:56:35 +0000

    services with cors origin error

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArticleService } from 'src/service/article.service';
+import { Article } from 'src/model/Aricle';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  articles: Article[];
 
+  constructor(private articleService: ArticleService) {}
+
+  ngOnInit(): void {
+    this.articleService.getArticles()
+      .subscribe((articles: Article[]) => {
+        console.log('okokok')
+        console.log(JSON.stringify(articles));
+        this.articles = articles;
+    }, 
+    error => console.log(JSON.stringify(error)));
+  }
 }
